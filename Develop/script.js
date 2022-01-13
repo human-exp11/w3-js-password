b26811a01dcc5fbf53ccfa8d6978d3866481721f
Luca Urbanowicz <human.exp11@gmail.com>
2022-01-13 14:16:54 -0700

    Addended README and added comments to JS

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Create variables for accepted characters
 var accSpecChar = ["!" ,"#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", " < ", "=", " > ", " ? ", "@", "[", "\|",  "]", "^",  "_", "{ ", "|", "}", "~"];
 var accLowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var accUpperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "S", "T", "U", "V", "W", "X", "Y", "Z"];
@@ -17,54 +18,52 @@ let accLength
 
 // Write password to the #password input
 function writePassword() {
-//ask user
-
-
-userLength ();
-
-
-var password = generatePassword();
-var passwordText = document.querySelector("#password");
-passwordText.value = password;
-
+    userLength();
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
 }
+
+
 var userLength = function () {
-     accLength = parseInt(prompt('How many characters long would you like your password to be?', 8));
-        if (accLength < 8) {
-            alert("Please choose more characters.");
-            userLength ();
-        }
-        else if (accLength > 128) {
-            alert("Please choose less characters.");
-            userLength ();
-        }
-        else {
-            followUp ();
-        }
-        return;
- }  
+    accLength = parseInt(prompt('How many characters long would you like your password to be?', 8));
+    if (accLength < 8) {
+        alert("Please choose more characters.");
+        userLength();
+    }
+    else if (accLength > 128) {
+        alert("Please choose less characters.");
+        userLength();
+    }
+    else {
+        followUp();
+    }
+    return;
+}  
 
- var followUp = function () {
-     userSpec = confirm("Would you like to include special character?");
-     userLower = confirm("Would you like to include lowercase letters?");
-     userUpper = confirm("Would you like to include uppercase letters?");
-     userNumber = confirm("Would you like to include numbers?");
-     if (userSpec) {
+
+// Ask user if they want to include different character types
+var followUp = function () {
+    userSpec = confirm("Would you like to include special character?");
+    userLower = confirm("Would you like to include lowercase letters?");
+    userUpper = confirm("Would you like to include uppercase letters?");
+    userNumber = confirm("Would you like to include numbers?");
+    if (userSpec) {
         pwItems = pwItems.concat(accSpecChar);
-     }
-     if (userLower) {
+    }
+    if (userLower) {
         pwItems = pwItems.concat(accLowerCase);
-     }
-     if (userUpper) {
-         pwItems = pwItems.concat(accUpperCase);
-     }
-     if (userNumber) {
-         pwItems = pwItems.concat(accNumbers);
-     }
- }
+    }
+    if (userUpper) {
+        pwItems = pwItems.concat(accUpperCase);
+    }
+    if (userNumber) {
+        pwItems = pwItems.concat(accNumbers);
+    }
+}
 
-
- var generatePassword = function () {
+// Utilize Math.random to select random characters
+var generatePassword = function () {
     for (let i = 0; i<accLength; i++) {
         var randomIndex = Math.floor(Math.random()*pwItems.length)
         var randomChar = pwItems[randomIndex]; 
@@ -73,7 +72,6 @@ var userLength = function () {
     return password.join("");
 }
 
-
-// Add event listener to generate button
+// Add Event Listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # w3-js-password
 Password Generator
+
+
+
+***Password Generator:*** When asked to create a Password Generator with given index.html & style.css files, I utilized JavaScript to meet all of the acceptance criteria.
+
+## Completed Steps:
+
+* Applied a Query Selector to the generate id associated with the generateBtn variable
+* Assigned passwrod criteria to four different variables (special characters, lower case, upper case, & numbers)
+* Applied a Query Selector to the password id within the text area
+* Created a userLength function that prompted question to user
+* Used if, else if, & else to keep password length within the parameters of 8-128 characters
+* Alerted user to choose more or less characters depending on input
+* Created a followUp function that used confirm to ask user if they would like to include specific character types
+* Utilized concatination to add on accepted characters to the user's password
+* Created a generatePassword function with a For Loop that used Math.random to populate a random password based off of the character length & character types the user chose
+* Added an Event Listenter that was associated with the event, "click" and the function writePassword
+* Created README file with link & screenshot
+
+[Live-site](https://human-exp11.github.io/portfolio/)
+
+![screenshot](./git-images/screenshotportfolio.png)
